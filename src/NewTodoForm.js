23ec4a84c8f631de4367026ef58e900438fc3c93
shannon7777 <shannonsimoncherry@gmail.com>
2019-09-24 03:15:42 +0800

    first commit

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -31,7 +31,7 @@ class NewTodoForm extends Component{
                     name='task'
                     value={this.state.task} 
                     onChange={this.handleChange} />
-                <button>Add Todo</button>
+                <button>Add todo</button>
             </form>
         );
     }
